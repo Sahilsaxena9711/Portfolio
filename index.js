1da186be45a3bff81f711553f928f47b7dbeb903
Sahilsaxena9711 <sahilsaxena9711@gmail.com>
2024-11-23 19:29:54 +0530

    [basic portfolio created]

---
+++ index.js
@@ -0,0 +1,31 @@
+function onMarkActive(e) {
+  let projects = [
+    "Underreacted",
+    "HtmlViewer",
+    "Blacker",
+    "Beatsy",
+    "Slacktivism",
+    "Supplymint",
+  ];
+  let current = e.target.id;
+  projects.map((_) => {
+    let element = document.getElementsByClassName(_);
+    let tabElement = document.getElementById(_);
+    if (_ == current) {
+      tabElement.classList.add("active");
+
+      element[0].classList.add("dblock");
+      element[0].classList.remove("dnone");
+    } else {
+      tabElement.classList.remove("active");
+
+      element[0].classList.add("dnone");
+      element[0].classList.remove("dblock");
+    }
+  });
+}
+
+function loadFunc() {
+  // let load = document.getElementById("loading");
+  // load.style.display = "none";
+}
